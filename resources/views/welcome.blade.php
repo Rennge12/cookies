621b3b6b8220d6c14da28d3dab2d3fa83868338b
ipb21.r.steins <ipb21.r.steins@pjvt.local>
2023-12-08 13:50:37 +0200

    cookies

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Sākums</h1>
+    <div class="cookie-banner">
+    <div class="cookie-content">
+        <p>This website uses cookies to enhance your experience. Would you like to accept them?</p>
+        <button class="cookie-btn">Accept</button>
+        <button class="cookie-btn cookie-decline">Decline</button>
+    </div>
+</div>
+</body>
+</html>
